df8cdc4e3192468a9ab20d605ff1ab764e5c73d9
Kamiltlusty <k.tlusty@student.uksw.edu.pl>
2025-07-10 23:15:29 +0200

    Shop checkboxes are now available

--- household-budget/public/src/index.js
+++ household-budget/public/src/index.js
@@ -13,6 +13,7 @@ header.innerHTML = `
         </button>
     </div>
     `
+document.body.appendChild(header);
 
 Date.prototype.myMonth = function () {
     const months = [
@@ -22,6 +23,14 @@ Date.prototype.myMonth = function () {
     return months[this.getMonth()];
 };
 
+const shops = [
+    "Lidl", "Biedronka", "Żabka", "Auchan", "Carrefour"
+];
+
+function shop(snum) {
+    return shops[snum];
+}
+
 const currentDate = new Date();
 const dateTime = currentDate.getDate() + " " + currentDate.myMonth() + " " + currentDate.getFullYear();
 
@@ -29,10 +38,37 @@ const section = document.createElement("section")
 section.className = "middle-bar"
 section.innerHTML = `
     <div class="shopping-form">
-    <h2>${dateTime}</h2>
+    <h2 class="shopping-date">${dateTime}</h2>
+    <h2 class="buyer">Kupujący: <input class="buyer-text-field" type="text" placeholder="Imię"></h2>    
+    <section class="shops"></section>    
     </div>
     `
-
-
-document.body.appendChild(header);
 document.body.appendChild(section);
+
+const shopsSection = document.querySelector(".shops");
+printShopsWithCheckboxes();
+
+function printShopsWithCheckboxes() {
+    for (i = 0; i < shops.length; i++) {
+        const cb = document.createElement("input");
+        cb.className = "checkbox"
+        cb.value = i;
+        cb.type = "checkbox";
+        const text = document.createElement("span");
+        text.innerText = shop(i);
+        shopsSection.appendChild(cb);
+        shopsSection.appendChild(text);
+    }
+}
+
+const checkboxes = document.querySelectorAll(".checkbox")
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener("click", function (event) {
+        let el = event.target.value;
+        checkboxes.forEach(cb => {
+            if (cb.value !== el) {
+                cb.checked = false;
+            }
+        })
+    });
+})
